8d792878a10cafae2e05363cd4921447ef31acc1
Joshua <james22@sheridancollege.ca>
2025-03-06 15:35:13 -0500

    Glyph puzzle working
    Glyph puzzle set up, only needs keyword and chosen stationary images.

--- Assets/Scripts/Glyph_Selector.cs
+++ Assets/Scripts/Glyph_Selector.cs
@@ -6,28 +6,33 @@ using UnityEngine.UI;
 public class Glyph_Selector : MonoBehaviour
 {
     public List<Sprite> glyphList;
-    public Image glyphDisplay;
-
-    int index = 0;
+    public List<Image> glyphDisplay;
+    public List<int> currentIndex;
 
     void Start()
     {
-        glyphDisplay.sprite = glyphList[index];
+        glyphDisplay[0].sprite = glyphList[currentIndex[0]];
+        glyphDisplay[1].sprite = glyphList[currentIndex[1]];
+        glyphDisplay[2].sprite = glyphList[currentIndex[2]];
+        glyphDisplay[3].sprite = glyphList[currentIndex[3]];
+        glyphDisplay[4].sprite = glyphList[currentIndex[4]];
+        glyphDisplay[5].sprite = glyphList[currentIndex[5]];
+        glyphDisplay[6].sprite = glyphList[currentIndex[6]];
     }
 
-    public void nextImage()
+    public void nextImage(int index)
     {
-        if (index == glyphList.Count - 1)
-            index = -1;
-        index += 1;
-        glyphDisplay.sprite = glyphList[index];
+        if (currentIndex[index] == glyphList.Count - 1)
+            currentIndex[index] = -1;
+        currentIndex[index] += 1;
+        glyphDisplay[index].sprite = glyphList[currentIndex[index]];
     }
 
-    public void previousImage()
+    public void previousImage(int index)
     {
-        if (index == 0)
-            index = glyphList.Count;
-        index -= 1;
-        glyphDisplay.sprite = glyphList[index];
+        if (currentIndex[index] == 0)
+            currentIndex[index] = glyphList.Count;
+        currentIndex[index] -= 1;
+        glyphDisplay[index].sprite = glyphList[currentIndex[index]];
     }
 }
